638ab5a8d2af0b61abeb1a410f75e1b4d7e10606
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 13:16:55 -0500

    basic auth structure

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ end
 
 gem 'jquery-rails'
 
+gem 'twitter'
+gem 'omniauth-twitter'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Techinterview::Application.routes.draw do
   resources :questions
   root to: 'questions#show'
+
+  # Omniauth Routes
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
+
 end
