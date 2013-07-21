037d62df483364be52bffa2149b665e61132a917
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 10:49:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techinterview::Application.config.secret_token = '6d0847e96f645c2cb035f52bb8f50318c80d08f3dcf32852c361097df78233f024ab23e01681d3a5caac92dfb2cece860c001ed12667bcc8a82a8d27b45b264a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Techinterview::Application.config.session_store :cookie_store, key: '_techinterview_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Techinterview::Application.config.session_store :active_record_store
