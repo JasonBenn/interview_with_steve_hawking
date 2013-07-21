9a876c0fd419dfa272960a2e97edae57afb4e695
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-21 13:44:39 -0500

    fixed signout link and @user variable in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def show
-    @user = User.find(session[:user_id])
+    @user = current_user
     if id = params[:id]
       @question = Question.find(id)
       render :show, layout: false
