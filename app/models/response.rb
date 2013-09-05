22ef2040378f8d55cdda8e8b924764c7cb655e33
Jason Benn <jasoncbenn@gmail.com>
2013-09-04 19:39:37 -0700

    Correct responses/questions association.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,5 @@
 class Response < ActiveRecord::Base
-
+	belongs_to :question
   attr_accessible :description, :question_id, :times_seen, :user_id
-
   validates_uniqueness_of :user_id, scope: [:question_id]
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130721180241) do
+ActiveRecord::Schema.define(:version => 20130721212519) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(:version => 20130721180241) do
     t.integer  "subcategory_id"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "audio_path"
+  end
+
+  create_table "responses", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.integer  "rating"
+    t.integer  "times_seen"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "subcategories", :force => true do |t|
